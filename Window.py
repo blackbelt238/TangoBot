e1be550532fabf585deb5b49d0ccd14feb07daf0
Ryan Brand <UNA8949@gmail.com>
2018-02-14 01:08:18 -0700

    Fixed implementation

--- Window.py
+++ Window.py
@@ -1,24 +1,27 @@
 from tango import Tango
+from tkinter import *
+import os
+
 class InputHandler:
     def __init__(self):
         os.system('xset r off')
         self.tango = Tango() # Tango instance to issue commands
-        self.root = tkinter.Tk()
+        self.root = Tk()
+        self.root.bind('<KeyPress>', self.method_name)
+        self.root.bind('<KeyRelease>', self.method_name)
 
     # Determine what to move from the key pressed.
     def input_pressed(key):
         print('Pressed', key.char)
         if key.char is 'w':
-            self.tango.drive(False)
-        elif key.char is 's':
             self.tango.drive(True)
+        elif key.char is 's':
+            self.tango.drive(False)
         elif key.char is ' ':
             self.tango.stop()
 
     # Slow down the robot on key release
     def input_released(key):
         print("Released", key.char)
-    self.root.bind('<KeyPress>', method_name)
-    self.root.bind('<KeyRelease>', method_name)
 
 start = InputHandler()
