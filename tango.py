78557844bdde741ba7a37d3c555d8c02bbcf84ef
Ethan Peterson <cap2010.ep@gmail.com>
2018-02-13 18:59:36 -0700

    Enable Tango to tilt/turn head

--- tango.py
+++ tango.py
@@ -18,18 +18,32 @@ class Tango:
     # drive allows the tango bot to drive forwards or backwards at 3 different speeds
     def drive(self, direction):
         pos = self.tango.position(SAME) # determine the wheels' current position
-        inc = TO_EXT / 3 # define the increments by which to accelerate
+        inc = TO_EXT / 3 # accelerate with 3 speeds
 
         # backward direction
-        if direction < 0:
+        if direction == False:
             # negatively accelerate the tango bot as long as it is not moving at its' maximum backward speed
             if pos < CENTER + TO_EXT:
                 self.tango.accelerate(SAME, pos + inc)
         # forward direction
-        elif direction > 0:
+        elif direction == True:
             # positively accelerate the tango bot as long as it is not moving at its' maximum forward speed
             if pos > CENTER - TO_EXT:
                 self.tango.accelerate(SAME, pos - inc)
+
+    # head allows the Tango bot's head to turn or tilt with 5 degrees of resolution
+    def head(self, port, direction):
+        pos = self.tango.position(port) # determine the head's current position on the desired axis
+        inc = TO_EXT / 5 # turn on 5 degrees/steps of resolution
+
+        # tilt the head down / turn it left TODO: CONFIRM THIS
+        if direction == False:
+            if pos > CENTER - TO_EXT:
+                self.tango.bendTurn(port, pos - inc)
+        # tilt the head down / turn it right TODO: CONFIRM THIS
+        elif direction == True:
+            if pos < CENTER + TO_EXT:
+                self.tango.bendTurn(port, pos + inc)
 
     # stop brings a moving Tango bot to a stop
     def stop(self):
