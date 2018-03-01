304eece00da1307ae473bfde3f6ab4395104aab5
Ryan Brand <UNA8949@gmail.com>
2018-03-01 12:34:30 -0700

    Created skeleton for how window and functionality will work

--- GUI/window.py
+++ GUI/window.py
@@ -1,5 +1,42 @@
 from Tkinter import *
 
+class NestedWindow():
+    def __init__(self, part, r):
+        self.window = Toplevel(r)
+        self.create_buttons(part)
+
+    def create_buttons(self, robo_part):
+        if robo_part == 'head':
+            self.head_buttons()
+
+    def head_buttons(self):
+        leftTwo = Button(self.window, text = '< <', command = self.destroy_window)
+        leftTwo.grid(row=2, column=0)
+
+        leftOne = Button(self.window, text = ' < ', command = self.destroy_window)
+        leftOne.grid(row=2, column=1)
+
+        upTwo = Button(self.window, text = '^\n^', command = self.destroy_window)
+        upTwo.grid(row=0, column=2)
+        
+        upOne = Button(self.window, text = '^\n', command = self.destroy_window)
+        upOne.grid(row=1, column=2)
+
+        downTwo = Button(self.window, text = '^\n^', command = self.destroy_window)
+        downTwo.grid(row=4, column=2)
+        
+        downOne = Button(self.window, text = '^\n', command = self.destroy_window)
+        downOne.grid(row=3, column=2)
+
+        rightOne = Button(self.window, text = ' > ', command = self.destroy_window)
+        rightOne.grid(row=2, column=3)
+        
+        rightTwo = Button(self.window, text = '> >', command = self.destroy_window)
+        rightTwo.grid(row=2, column=4)
+
+    def destroy_window(self):
+        self.window.destroy()
+
 class BlockWindow():
     def __init__(self, r, c=None):
         self.client = c
@@ -10,20 +47,19 @@ class BlockWindow():
         self.canvasH = 530
         self.c = Canvas(self.root, width = self.canvasW, height = self.canvasH)
         self.create_buttons()
-        self.c.pack()
 
     def pressed(self):
         print 'test'
 
-    def create_window(self):
-        window = Toplevel(self.root)
+    def make_nested(self, robo_part):
+        window = NestedWindow(robo_part, self.root)
 
     def create_buttons(self):
-        button1 = Button(self.root, text = 'press', command = self.create_window)
-        button1.pack(side="top", expand=True, padx=4, pady=4)
+        head = Button(self.root, text = 'Head', command = lambda: self.make_nested('head'))
+        head.place(x=0,y=0)
 
-        button2 = Button(self.root, text = 'press2', command = self.pressed)
-        button2.pack(side="top", expand=True, padx=4, pady=4)
+        torso = Button(self.root, text = 'Torso', command = self.pressed)
+        torso.place(x=0,y=100)
 def main():
     root = Tk()
     gui = BlockWindow(root)
