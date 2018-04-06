abfbe8968cb2a8b12126ab313ad3c5173b454755
Ethan Peterson <cap2010.ep@gmail.com>
2018-04-06 00:34:39 -0600

    Hold instance of ActionQueue in the server.

--- server.py
+++ server.py
@@ -3,30 +3,26 @@ import socket
 class Server:
     ''' Server allows the standing up of a server to listen for connections, echoing back any message it recieves '''
     saddr = (socket.gethostname(), 9999) # create the server address using the local machine name
+    actionqueue = None                   # the ActionQueue the server is running alongside
 
-    def startServer():
+    def start():
         ''' startServer stands up an echo server '''
         serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create a TCP/IP socket object
         serversocket.bind(Server.saddr)                                  # bind to the port
         serversocket.listen(5)                                           # listen for up to 5 incoming connections
 
-        while True:
-            # wait for a connection
-            clientsocket, addr = serversocket.accept()
-            try:
+        try:
+            while True:
+                # wait for a connection
+                clientsocket, addr = serversocket.accept()
                 print('connection from', addr)
 
-                # Receive the data in small chunks and retransmit it
-                while True:
-                    data = clientsocket.recv(16)
-                    print('received {}'.format(data.decode('ascii')))
-                    if data:
-                        print('sending data back to the client')
-                        clientsocket.sendall(data)
-                    else:
-                        print('no data from', addr)
-                        break
-            finally:
-                # Clean up the connection no matter what
-                clientsocket.close()
-Server.startServer()
+                # recieve a message
+                msg = clientsocket.recv(1024).decode('ascii')
+                # see if it matches any predetermined commands
+                if msg == 'start':
+                    Server.actionqueue.execute()
+        finally:
+            # Clean up the connection no matter what
+            clientsocket.close()
+Server.start()
